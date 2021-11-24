f15de666c9ec9e02988a3dad398f433b951af172
Suyeon <koo5717@naver.com>
2021-11-25 00:12:26 +0900

    [Feature/suyeon-5] 회원가입 XML 구현 (#6)
    * fix: .gitignore에 gradle, android studio 추가
    
    * refactor: Night theme 삭제 및 배경색 전역 적용
    
    * feat: colors.xml 색상 추가 및 upper Case로 변경
    
    * feat: include_toolbar.xml 생성
    
    * feat: Button background drawable 생성
    
    * feat: activity_sign_up9.xml 구현
    
    * feat: activity_sign_up6.xml 구현
    
    * feat: Base button selector 생성
    
    * feat: EditText background drawable 생성
    
    * feat: Base editText, button style 전역 적용
    
    * feat: activity_sign_up7.xml 구현
    
    * fix: Button style 그림자 제거
    
    * feat: activity_sign_up5.xml 구현
    
    * fix: Button style paddingStart 16dp -> 8dp로 변경
    
    * feat: activity_sign_up8.xml 구현
    
    * feat: strings.xml에 string 추가
    
    * fix: item_sign_up6.xml maxLine 적용
    
    * feat: SignActivity5~9 AndroidManifest에 등록
    
    * fix: .gitignore에 codeStyles 추가
    
    * fix: activity_sign_up8.xml id명 변경 안 된 부분 변경
    
    * feat: array.xml 생성 및 재학 상태 추가
    
    * feat: Spinner background drawable 생성
    
    * fix: activity_sign_up8.xml에 spinner 추가
    
    - 텍스트 스타일 수정 및 아이템 xml 추가 필요!!

---
+++ app/src/main/java/com/sys/timmo/SignUpActivity5.kt
@@ -0,0 +1,11 @@
+package com.sys.timmo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up5)
+    }
+}
---
+++ app/src/main/java/com/sys/timmo/SignUpActivity9.kt
@@ -0,0 +1,11 @@
+package com.sys.timmo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity9 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up9)
+    }
+}
