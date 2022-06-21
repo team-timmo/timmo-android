9d28474eb26ba586b71b03fa017855da2f191fbd
soo5717 <koo5717@naver.com>
2022-06-22 00:10:36 +0900

    :sparkles: BaseActivity.kt, BaseFragment.kt 구현

---
+++ app/src/main/java/com/sys/timmo/base/BaseFragment.kt
@@ -0,0 +1,30 @@
+package com.sys.timmo.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<viewBinding : ViewBinding> : Fragment() {
+
+    private var _binding: viewBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = getFragmentBinding(inflater, container)
+        return binding.root
+    }
+
+    abstract fun getFragmentBinding(inflater: LayoutInflater, container: ViewGroup?): viewBinding
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
