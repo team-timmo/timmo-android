b86c26c9e42ea297b452da5889f2aaba3ce10f9a
Suyeon <koo5717@naver.com>
2022-06-23 23:48:49 +0900

    ✨ [Feature/16] 회원가입 XML 구현 (#19)
    * :speech_balloon: 회원가입 문자열 추가
    
    * :bento: 회원가입 아이콘 및 로고 추가
    
    * :lipstick: EditText, Spinner drawable 생성
    
    * :lipstick: BaseSpinner, SearchEditText, TextButton 스타일 추가 및 적용
    
    * :sparkles: activity_sign_up5.xml 구현
    
    * :sparkles: activity_sign_up6.xml 구현
    
    * :sparkles: activity_sign_up7.xml 구현
    
    * :sparkles: activity_sign_up8.xml 구현
    
    * :sparkles: activity_sign_up9.xml 구현

---
+++ app/src/main/java/com/sys/timmo/presentation/signup/SignUpActivity5.kt
@@ -0,0 +1,12 @@
+package com.sys.timmo.presentation.signup
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sys.timmo.R
+
+class SignUpActivity5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up5)
+    }
+}
